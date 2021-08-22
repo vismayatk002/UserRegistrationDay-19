a27e6401c9723bec41030a96354937e4b455b46a
vismayatk002 <vismayatk002@gmail.com>
2021-08-22 14:58:33 +0530

    Refactor validate mobile number

--- src/com/userregistration/UserRegistration.java
+++ src/com/userregistration/UserRegistration.java
@@ -1,6 +1,7 @@
 package com.userregistration;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class UserRegistration {
 	
@@ -21,6 +22,33 @@ public class UserRegistration {
 		}
 		return returnFlag;
 	}
+	
+	public boolean validateEmail(String email) {
+			
+		String regex = "^[A-Za-z](.+)@(.+)$"; 
+		Pattern pattern = Pattern.compile(regex); 
+		if(pattern.matcher(email).matches()) {
+			System.out.println( "True");
+			return true;
+		}
+		else {
+			System.out.println( "False");
+			return false;
+		}
+	}
+	
+	public boolean validateMobileNo(String mobNo) {
+		
+		Pattern pattern = Pattern.compile("91[0-9]{10}");
+		if(pattern.matcher(mobNo).matches()) {
+			return true;
+		}
+		else {
+			System.out.println( "Invalid Mobile number");
+			return false;
+		}
+	}
+
     public static void main( String[] args ) {
         
     	UserRegistration user = new UserRegistration();
@@ -31,7 +59,30 @@ public class UserRegistration {
     		System.out.print( "Enter your last name : ");
     		String lastName = sc.nextLine(); 
     		user.validateName(lastName);
+    		System.out.print( "Enter your Mobile Number : ");
+    		String mobNo = sc.nextLine(); 
+    		user.validateMobileNo(mobNo);
     	}
+    	
+    	String emailId1 = "abc@yahoo.com";
+    	String emailId2 = "abc-100@yahoo.com";
+    	String emailId3 = "abc.100@yahoo.com";
+    	String emailId4 = "abc111@abc.com";
+    	String emailId5 = "abc-100@abc.net";
+    	String emailId6 = "abc.100@abc.com.au";
+    	String emailId7 = "abc@1.com";
+    	String emailId8 = "34abc@gmail.com.com";
+    	String emailId9 = ".abc+100@gmail.com";
+    	user.validateEmail(emailId1);
+    	user.validateEmail(emailId2);
+    	user.validateEmail(emailId3);
+    	user.validateEmail(emailId4);
+    	user.validateEmail(emailId5);
+    	user.validateEmail(emailId6);
+    	user.validateEmail(emailId7);
+    	user.validateEmail(emailId8);
+    	user.validateEmail(emailId9);
+    	
     	sc.close();
     }
 }
